2227eac9f9b9ba8eab9e07cce9c41310c07096d4
Michael Owuor <michael.owuor@gmail.com>
2022-08-22 16:31:17 +0300

    feat: create button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,15 @@
 import React from 'react';
 
-const Button = () => {
-	return <div></div>;
+const Button = ({ styles }) => {
+	return (
+		/* button component start */
+		<button
+			type="button"
+			className={`bg-blue-gradient rounded-[10px] py-4 px-6 font-poppins text-[18px] font-medium text-primary outline-none ${styles}`}
+		>
+			Get Started
+		</button>
+		/* button component end */
+	);
 };
 export default Button;
